68626185e7e4ba0b06c5ed9d4e5f6fb15969bce1
Lars BOLLMANN <lars.bollmann@ist.ac.at>
2022-03-09 09:22:06 +0100

    added functionality for cross maze data

--- function_files/load_data.py
+++ function_files/load_data.py
@@ -38,7 +38,7 @@ class LoadData:
        - saves/loads data in form of a python dictionary --> data: location, cell spike timings etc.
     """
 
-    def __init__(self, session_name, experiment_phase, cell_type, pre_proc_dir):
+    def __init__(self, session_name, experiment_phase, pre_proc_dir):
         # --------------------------------------------------------------------------------------------------------------
         # creates SelectedData object
         #
@@ -77,19 +77,6 @@ class LoadData:
         # if two experiment phases are supposed to be analyzed
         self.experiment_phase = experiment_phase
         self.nr_phases = len(experiment_phase)
-
-        # --------------------------------------------------------------------------------------------------------------
-        # check if only one or more populations are supposed to be returned
-        # --------------------------------------------------------------------------------------------------------------
-
-        if len(cell_type) == 1:
-            # if only one cell type is provided --> analyzing one region
-            self.cell_type = cell_type[0]
-            self.nr_pop = 1
-        else:
-            # if two cell types are provided --> analyze both regions
-            self.cell_type = cell_type
-            self.nr_pop = len(cell_type)
 
         # --------------------------------------------------------------------------------------------------------------
         # data file name --> file is stored in /temp_data_old subdirectory
@@ -171,7 +158,7 @@ class LoadData:
         #                                                  (SLEEP_TYPE can be "rem", "nrem", "sws")
         #
         # --------------------------------------------------------------------------------------------------------------
-    
+
         print("LOADING LFP FILE ...")
         # initialize empty dictionary
         data_dic_list = []
@@ -185,10 +172,10 @@ class LoadData:
             infile.close()
 
             # check if lfp data exists as pickle --> if not, create pickled data
-            if not os.path.isfile(self.pre_proc_dir+"lfp/" + self.file_name + "_" + exp_phase_id):
+            if not os.path.isfile(self.pre_proc_dir + "lfp/" + self.file_name + "_" + exp_phase_id):
                 self.save_lfp_data(exp_phase_id, exp_phase_description)
             # loading the data
-            infile = open(self.pre_proc_dir+"lfp/" + self.file_name + "_" + exp_phase_id, 'rb')
+            infile = open(self.pre_proc_dir + "lfp/" + self.file_name + "_" + exp_phase_id, 'rb')
             lfp_data_dic = pickle.load(infile)
             infile.close()
             data_dic.update(lfp_data_dic)
@@ -284,7 +271,7 @@ class LoadData:
                         cell_ids_ss_1 = [i for i in cell_ids if i <= self.cell_type_sub_div_dic[cell_type][0] + 2]
                         cell_ids_ss_2 = [i for i in cell_ids if i > self.cell_type_sub_div_dic[cell_type][0] + 2]
                         data_dic["trial_data"]["trial" + str(trial_id)]["spike_times"][self.
-                                 cell_type_sub_div_dic[cell_type][1]] = {}
+                            cell_type_sub_div_dic[cell_type][1]] = {}
                         data_dic["trial_data"]["trial" + str(trial_id)]["spike_times"][self.
                             cell_type_sub_div_dic[cell_type][2]] = {}
                         # get spike data
@@ -350,6 +337,67 @@ class LoadData:
             #         "info": {"new_rule_trial": new_rule_trial, "rule_order": rule_identifier}
             #     }
 
+        elif "cross_maze_task" in data_description:
+            """ --------------------------------------------------------------------------------------------------------
+            # this section is used for cross maze task: split data into trials, save additional data such as success/fail
+            # -------------------------------------------------------------------------------------------------------"""
+
+            print(" - processing cross maze task")
+
+            # start,centrebegin, centreend, goalbegin, goalend, startarm, goalarm, control, lightarm, ruletype, errortrial
+            timestamps = np.loadtxt(
+                data_dir + "/" + session_name + "/" + session_name + "_" + exp_phase + ".timestamps").astype(int)
+
+            # get start and end timestamp of each trial
+            start_times = timestamps[:, 0]
+            end_times = timestamps[:, 4]
+
+            data_dic["trial_data"] = {}
+
+            # go through all trials and get location and spike data
+            for trial_id, (s_t, e_t) in enumerate(zip(start_times, end_times)):
+                data_dic["trial_data"]["trial" + str(trial_id)] = {}
+                """ ----------------------------------------------------------------------------------------------------
+                get location data for the trial
+                ---------------------------------------------------------------------------------------------------- """
+                data_dic["trial_data"]["trial" + str(trial_id)]["whl"] = whl[s_t:e_t]
+
+                """ ----------------------------------------------------------------------------------------------------
+                get spike data for the trial
+                ---------------------------------------------------------------------------------------------------- """
+                data_dic["trial_data"]["trial" + str(trial_id)]["spike_times"] = {}
+                # go through all cell types and get spike times
+                for cell_type in cell_type_array:
+                    # check if cells need to be split into two populations (defined in parameter file)
+                    if self.cell_type_sub_div_dic and cell_type in self.cell_type_sub_div_dic:
+                        cell_ids = self.get_cell_id(data_dir, session_name, cell_type)
+                        # split into two subsets using the provided cutoff
+                        cell_ids_ss_1 = [i for i in cell_ids if i <= self.cell_type_sub_div_dic[cell_type][0] + 2]
+                        cell_ids_ss_2 = [i for i in cell_ids if i > self.cell_type_sub_div_dic[cell_type][0] + 2]
+                        data_dic["trial_data"]["trial" + str(trial_id)]["spike_times"][self.
+                            cell_type_sub_div_dic[cell_type][1]] = {}
+                        data_dic["trial_data"]["trial" + str(trial_id)]["spike_times"][self.
+                            cell_type_sub_div_dic[cell_type][2]] = {}
+                        # get spike data
+                        data_ss_1 = self.load_spikes_for_interval(cell_IDs=cell_ids_ss_1, clu=clu, res=res,
+                                                                  start_interval=s_t, end_interval=e_t)
+                        data_ss_2 = self.load_spikes_for_interval(cell_IDs=cell_ids_ss_2, clu=clu, res=res,
+                                                                  start_interval=s_t, end_interval=e_t)
+                        # write to dictionary (one key for each cell type)
+                        data_dic["trial_data"]["trial" + str(trial_id)]["spike_times"][self.
+                            cell_type_sub_div_dic[cell_type][1]] = data_ss_1
+                        data_dic["trial_data"]["trial" + str(trial_id)]["spike_times"][self.
+                            cell_type_sub_div_dic[cell_type][2]] = data_ss_2
+                    # if cells are not supposed to be split --> save everything for one cell type entry
+                    else:
+                        data_dic["trial_data"]["trial" + str(trial_id)]["spike_times"][cell_type] = {}
+                        cell_ids = self.get_cell_id(data_dir, session_name, cell_type)
+                        # get spike data
+                        data = self.load_spikes_for_interval(cell_IDs=cell_ids, clu=clu, res=res, start_interval=s_t,
+                                                             end_interval=e_t)
+                        # write to dictionary
+                        data_dic["trial_data"]["trial" + str(trial_id)]["spike_times"][cell_type] = data
+
         """ --------------------------------------------------------------------------------------------------------
         # for all data: load spike times for all cell types for the entire data without splitting into trials
         # -------------------------------------------------------------------------------------------------------"""
@@ -408,14 +456,19 @@ class LoadData:
                 data_dic[sleep_part] = {}
                 if not os.path.isfile(data_dir + "/" + session_name + "/" + session_name + "_" + exp_phase + "." +
                                       sleep_part) or os.path.getsize(data_dir + "/" + session_name + "/" +
-                                      session_name + "_" + exp_phase + "." + sleep_part) == 0:
-                    print("   --> SLEEP PART "+sleep_part+" FILE NOT FOUND:")
-                    print("       "+data_dir + "/" + session_name + "/" + session_name + "_" + exp_phase + "." +
-                                      sleep_part)
+                                                                     session_name + "_" + exp_phase + "." + sleep_part) == 0:
+                    print("   --> SLEEP PART " + sleep_part + " FILE NOT FOUND:")
+                    print("       " + data_dir + "/" + session_name + "/" + session_name + "_" + exp_phase + "." +
+                          sleep_part)
                     continue
                 # get time intervals for sleep part
-                timestamps = np.loadtxt(data_dir + "/" + session_name + "/" + session_name
-                                        + "_" + exp_phase + "." + sleep_part).astype(int)
+                try:
+                    timestamps = np.loadtxt(data_dir + "/" + session_name + "/" + session_name
+                                            + "_" + exp_phase + "." + sleep_part).astype(int)
+                # value missing
+                except:
+                    timestamps = np.genfromtxt(data_dir + "/" + session_name + "/" + session_name
+                                               + "_" + exp_phase + "." + sleep_part).astype(int)
 
                 # save timestamps of sleep for time binning
                 # if timestamp has only one entry --> expand dimension
@@ -435,7 +488,23 @@ class LoadData:
             # ----------------------------------------------------------------------------------------------------------
             data_dic["start_box"] = self.data_params_dictionary["start_box_coordinates"]
 
-        filename = self.pre_proc_dir + session_name+"_" + exp_phase
+        elif "cross_maze_task" in data_description:
+            # ----------------------------------------------------------------------------------------------------------
+            # get additional cross_maze_task data --> start_arm, rule etc.
+            # ----------------------------------------------------------------------------------------------------------
+            # start,centrebegin, centreend, goalbegin, goalend, startarm, goalarm,
+            # control, lightarm, ruletype, errortrial
+            trial_info = np.loadtxt(data_dir + "/" + session_name + "/" + session_name + "_" +
+                                    exp_phase + ".timestamps").astype(int)
+            data_dic["timestamps"] = trial_info[:, :5]
+            data_dic["start_arm"] = trial_info[:, 5]
+            data_dic["goal_arm"] = trial_info[:, 6]
+            data_dic["control"] = trial_info[:, 7]
+            data_dic["lightarm"] = trial_info[:, 8]
+            data_dic["ruletype"] = trial_info[:, 9]
+            data_dic["errortrial"] = trial_info[:, 10]
+
+        filename = self.pre_proc_dir + session_name + "_" + exp_phase
 
         outfile = open(filename, 'wb')
         pickle.dump(data_dic, outfile)
@@ -477,7 +546,7 @@ class LoadData:
         # --------------------------------------------------------------------------------------------------------------
         par_file = data_dir + "/" + session_name + "/" + session_name + "_" + exp_phase + ".par"
         par_list = []
-        with open(par_file,"r") as file:
+        with open(par_file, "r") as file:
             for line in file:
                 for par in line.split():
                     par_list.append(par)
@@ -509,10 +578,10 @@ class LoadData:
             print("   --> .eegh FILE NOT FOUND --> CONTINUE")
 
         # check if lfp folder exists, otherwise create it
-        if not os.path.exists(self.pre_proc_dir+"lfp/"):
-            os.makedirs(self.pre_proc_dir+"lfp/")
+        if not os.path.exists(self.pre_proc_dir + "lfp/"):
+            os.makedirs(self.pre_proc_dir + "lfp/")
 
-        filename = self.pre_proc_dir+"lfp/"+session_name+"_" + exp_phase
+        filename = self.pre_proc_dir + "lfp/" + session_name + "_" + exp_phase
 
         outfile = open(filename, 'wb')
         pickle.dump(data_dic, outfile, protocol=4)
@@ -566,7 +635,6 @@ class LoadData:
 
         # go through all cell ids that were provided
         for cell_ID in cell_IDs:
-
             # find all entries of the cell_ID in the clu list
             entries_cell = np.where(clu == cell_ID)
 
@@ -611,7 +679,6 @@ class LoadData:
 
         # go through all cell ids that were provided
         for cell_ID in cell_IDs:
-
             # find all entries of the cell_ID in the clu list
             entries_cell = np.where(clu == cell_ID)
 
@@ -650,7 +717,7 @@ class LoadData:
         return whl_interpol
 
     @staticmethod
-    def trial_timestamps_from_whl(whl_interpol,x_min_sb, x_max_sb, y_min_sb, y_max_sb,
+    def trial_timestamps_from_whl(whl_interpol, x_min_sb, x_max_sb, y_min_sb, y_max_sb,
                                   min_trial_length=8, min_sb_duration=2):
         """
         computes timestamps for single trials of cheese board task using times spend in the start box to separate
@@ -743,7 +810,7 @@ class LoadData:
             # start,centrebegin, centreend, goalbegin, goalend, startarm, goalarm, control, lightarm,
             # ruletype, errortrial
             if trial[9] in trial_sel["ruletype"] and trial[10] in trial_sel["errortrial"] and \
-            trial[5] in trial_sel["startarm"] and trial[6] in trial_sel["goalarm"]:
+                    trial[5] in trial_sel["startarm"] and trial[6] in trial_sel["goalarm"]:
                 trial_intervals.append(trial_ID)
                 # remember number of trials saved to identify rule switch case
                 temp += 1
@@ -765,12 +832,12 @@ class LoadData:
         # go through selected trials
         for trial_ID in trial_IDs:
             # create entry in dictionary
-            data["trial"+str(trial_ID)] = {}
+            data["trial" + str(trial_ID)] = {}
             # -----------------------------------------------
             # timestamps: 20kHz/512 --> 25.6 ms per time bin
             # res data: 20kHz
             # time interval for res data: timestamp data * 512
-            t_start = timestamps[trial_ID,0] * 512
+            t_start = timestamps[trial_ID, 0] * 512
             t_end = timestamps[trial_ID, 4] * 512
             # go through selected cells
             for cell_ID in cell_IDs:
@@ -783,7 +850,7 @@ class LoadData:
                 # only use spikes that correspond to time interval of the trial
                 cell_spikes_trial = [x for x in res[ind_res_file] if t_start < x < t_end]
                 # append data
-                data["trial" + str(trial_ID)]["cell"+str(cell_ID)] = cell_spikes_trial
+                data["trial" + str(trial_ID)]["cell" + str(cell_ID)] = cell_spikes_trial
 
         return data
 
@@ -804,7 +871,7 @@ class LoadData:
             # select locations that correspond to time interval of the trial
             loc_trial = whl_lin[t_start:t_end]
             # append data
-            loc["trial"+str(trial_ID)] = loc_trial
+            loc["trial" + str(trial_ID)] = loc_trial
 
         return loc
 
@@ -861,16 +928,13 @@ class LoadData:
             # select locations that correspond to time interval of the trial
             loc_trial = whl[t_start:t_end]
             # append data
-            loc["trial"+str(trial_ID)] = loc_trial
+            loc["trial" + str(trial_ID)] = loc_trial
 
         return loc
 
     """#################################################################################################################
     # Getter functions
     #################################################################################################################"""
-
-    def get_cell_type(self):
-        return self.cell_type
 
     def get_experiment_phase_id(self):
         return self.experiment_phase_id
